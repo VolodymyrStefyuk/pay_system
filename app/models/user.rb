cfece9a86ddfbcdf3f704ce7b9fdf2cddf6c5f90
Volodymyr <vova.stefyuk@gmail.com>
2022-12-22 20:38:02 +0200

    added validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+validates :name,  presence: true, length: { minimum: 4 }
+validates :password,  presence: true, confirmation: true, length: { in: 4..20 }
+validates :accountNumber,  presence: true, length: { is: 16 },  numericality: { only_integer: true }
 end
