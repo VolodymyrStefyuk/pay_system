2cef83a602882691e9cf67fdc9643318cd815172
Volodymyr <vova.stefyuk@gmail.com>
2022-12-23 08:50:53 +0200

    lab11

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  test "not null" do
-    assert 
+
+    test "user should be valid" do
+      assert User.new(name: "user1", password:"111111", accountNumber:9999999999999999).valid?
+      assert_not User.new(name: "us", password:"111111", accountNumber:9999999999999999).valid?
+      assert_not User.new(name: "user1", password:"", accountNumber:9999999999999999).valid?
+      assert_not User.new(name: "user1", password:"111111", accountNumber:9).valid?
+      assert_not User.new(name: "user1", password:"111111", accountNumber:"aaaaaaaaaaaaaaaa").valid?
     end
 end
